184fee0e3e2b2e0cd640b080695344fedc979b63
XK8 <xk-8@mail.ru>
2015-08-19 21:06:12 +0300

    fix

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ var server  = http.createServer(app);
 var io      = require('socket.io').listen(server);
 
 app.set('port', (process.env.PORT || 5000));
-app.set('host', (process.env.HOST || '127.0.0.1'));
-app.set('views', path.join(__dirname, 'views'));
+//app.set('host', (process.env.HOST || '127.0.0.1'));
+
+app.set('views', __dirname, '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon(path.join(__dirname,'public','favicon.ico')));
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.favicon(__dirname +'/public/favicon.ico'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(function(err, req, res, next) {
 	if(!err) return next();
 	console.log(err.stack);
@@ -31,5 +34,6 @@ app.post('/vote', routes.vote);
 io.sockets.on('connection', routes.vote);
 
 server.listen(app.get('port'), app.get('host'), function(){
-  console.log('App running on '+ app.get('host') + ':' + app.get('port'));
+//  console.log('App running on '+ app.get('host') + ':' + app.get('port'));
+  console.log('App running !!!');
 });
